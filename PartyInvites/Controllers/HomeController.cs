3edfcee0ef9b117d52da197ca64465b58ae51e19
Dennis Durairaj <dennis.durairaj@pl.pwc.com>
2019-04-03 16:11:23 +0200

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            var hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
